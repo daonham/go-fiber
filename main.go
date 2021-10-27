4682d42b067917b1848f19326da81095e1c8c5df
daonham <daonham95@gmail.com>
2021-10-28 00:03:19 +0700

    add: setup local

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, Accept-Encoding, X-Requested-With",
+		AllowMethods:     "GET,POST,PUT,DELETE,PATCH",
+		AllowCredentials: true,
+		ExposeHeaders:    "Content-Length",
+		MaxAge:           86400,
+	}))
+
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestCompression,
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":8080")
+}
